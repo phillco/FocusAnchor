478cfa0fb8084cbfa76fefb8dc92df510cb1a7f9
Phillip Cohen <pc@phillipcohen.net>
2010-02-22 15:59:46 +0000

    The main window is now hidden from alt+tab.

--- FocusAnchor/HoverForm.cs
+++ FocusAnchor/HoverForm.cs
@@ -5,14 +5,27 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace FocusAnchor
 {
     public partial class HoverForm : Form
     {
+        // Code needed to hide the window from Alt+Tab.
+        private const int GWL_EXSTYLE = ( -20 );
+        private const int WS_EX_TOOLWINDOW = 0x80;
+        private const int WS_EX_APPWINDOW = 0x40000;
+        [DllImport( "user32", CharSet = CharSet.Auto )]
+        private static extern int GetWindowLong( IntPtr hWnd, int nIndex );
+        [DllImport( "user32", CharSet = CharSet.Auto )]
+        private static extern int SetWindowLong( IntPtr hWnd, int nIndex, int dwNewLong );
+
         public HoverForm( )
         {
             InitializeComponent( );
+
+            // Hide this window from Alt+Tab.
+            SetWindowLong( this.Handle, GWL_EXSTYLE, ( GetWindowLong( this.Handle, GWL_EXSTYLE ) | WS_EX_TOOLWINDOW ) & ~WS_EX_APPWINDOW );
         }
 
         private void HoverForm_Shown( object sender, EventArgs e )
